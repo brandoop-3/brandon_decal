dfecffa82a015a4b3eeb5f33a2611d3bd8087688
Brandon <brandonp2003@berkeley.edu>
2025-02-07 20:13:35 -0800

    adding homework 2 file

--- homework2/hw2_done.py
+++ homework2/hw2_done.py
@@ -1,34 +1,42 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
 # The command that can take you back to ~/Desktop is "cd ~/Desktop"
 
+
 # 2) What does ~/ mean?
 # This means that  you are currently in your home directory 
+
 
 # 3) What's the difference between and abolute and a relative path in your terminal?
 # An absolute path is showing a location from the root directory, where the relative path is from the current working directory.
 
+
 # 4) What command would bring you back to your home directory?
 # To take you back to your hoem directory, you can call the demand "cd".
+
 
 # 5) If you called rm ./ in your current working directory, what would it do? 
 # It would attempt to remove all items and files in the current working directory. So if I was in python_decal, it would attempt to remove everything 
 # inside the directory besides the directory itself.
+
 
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
 # git add.  --  This command tells git to track changes to wahever file the command is applied to.
 # git commit -- saves the changes done to the file in the local repository
 # git push -- upload the changes to a remote repositroy like GitHub
 
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
 # After calling git add, a message pops up saying that the file being pushed is ready to committ.
+
 
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
 #I haven't encountered anymore bugs yet so I'm lucky :).
 
+
 # 9) How does cloning a repository differ from pulling from a repository?
 # Cloning a repository creates a local copy of the entire repository, while pulling from the repository updates your current local branch from a remote repository.
-#   
-# 10) Tell me a fun fact!
-# I've played 9 different sports.
 
-2.0 * 10^5 + 2.50347 *10^5
+
+# 10) Tell me a fun fact!
+# I've played 10 different sports each with over 3 years of consecutive playing.
+
